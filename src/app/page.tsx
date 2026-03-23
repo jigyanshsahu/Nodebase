7bc6880f753584048a07d063379a6dadc78015ab
Jigyansh sahu <indianinja008@gmail.com>
2026-03-23 20:06:51 +0530

    login sesion error solve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client"
+import { createAuthClient } from "better-auth/react"
+
 
 const page = () => {
+const { data } = authClient.useSession();
   return (
-    <div>
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(data)}
+      {
+        data && (
+      <Button onClick={()=> authClient.signOut()}>
+        Logout
+      </Button>
+          
+        )
+      }
       
     </div>
   )
